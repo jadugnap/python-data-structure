f893d995a63dcdfa61131990dc906a81d4a290b0
Jadug Parusa <jadug.parusa@shopee.com>
2020-06-21 08:36:50 +0800

    fix: Correct problem3_Heap_Tree_BFS/huffman_coding bug found from new test cases

--- problem3_Heap_Tree_BFS/huffman_coding.py
+++ problem3_Heap_Tree_BFS/huffman_coding.py
@@ -1,4 +1,4 @@
-import sys
+import sys, copy
 from collections import Counter, deque
 from heapq import heapify,heappush,heappop
 
@@ -66,6 +66,7 @@ class Tree():
         return self.root
 
     def traverse_huffman_code(self):
+        # BFS tree traversal
         level = 0
         q = Queue()
         visit_order = []
@@ -77,22 +78,33 @@ class Tree():
                 visit_order.append( (None,level,''))
                 continue
             visit_order.append( (node,level,code) )
+            # huffman_code[left] = '0'
             if node.has_left_child():
                 q.enq( (node.get_left_child(), level+1, code+'0') )
             else:
                 q.enq( (None, level+1, '') )
+            # huffman_code[right] = '1'
             if node.has_right_child():
                 q.enq( (node.get_right_child(), level+1, code+'1') )
             else:
                 q.enq( (None, level+1, '') )
         s = "Tree\n"
         previous_level = -1
+        # combine huffman_code[left] and huffman_code[right]
         huffman_code = {}
         for i in range(len(visit_order)):
             node, level, code = visit_order[i]
             if node and len(node.get_name())==1:
                 huffman_code[node.get_name()] = code
         return huffman_code
+
+    def hardcode_single_huffman_code(self):
+        # Set a tree where right_node = (deepcopy of) root_node = this single character
+        root_node = self.get_root()
+        right_node = copy.deepcopy(root_node)
+        root_node.set_right_child(right_node)
+        # hardcode the huffman_code[letter] = '1'
+        return {right_node.get_name(): '1'}
 
 def set_frequency_map(msg):
     # Set a map of frequency of each character in the message
@@ -114,31 +126,45 @@ def pop_minimum_pair(heap):
     return heap
 
 def huffman_encoding(msg, is_debug=False):
-    # Generate unique binary code for each character of our string message
+    # Encode each character by huffman_tree with unique prefix binary code
+    # for an empty character (edge case)
+    if len(msg)==0:
+        return '', None
     heap_of_tree = set_frequency_map(msg)
-    while len(heap_of_tree) > 1:
-        # print(f"heap_of_tree = {heap_of_tree}")
-        heap_of_tree = pop_minimum_pair(heap_of_tree)
-    # print(f"final heap_of_tree = {heap_of_tree}")
-    huffman_tree = heap_of_tree[0]
-    huffman_code = huffman_tree.traverse_huffman_code()
-    if is_debug:
-        print(f"debug: {huffman_code}")
+    # for a single character type (edge case)
+    if len(heap_of_tree) == 1:
+        if is_debug: print(f"heap_of_tree = {heap_of_tree}")
+        huffman_tree = heap_of_tree[0]
+        huffman_code = huffman_tree.hardcode_single_huffman_code()
+    # for multiple character types
+    else:
+        while len(heap_of_tree) > 1:
+            if is_debug: print(f"heap_of_tree = {heap_of_tree}")
+            heap_of_tree = pop_minimum_pair(heap_of_tree)
+        if is_debug: print(f"final heap_of_tree = {heap_of_tree}")
+        huffman_tree = heap_of_tree[0]
+        huffman_code = huffman_tree.traverse_huffman_code()
+    if is_debug: print(f"huffman_code: {huffman_code}")
     encoded_msg = ''
     for letter in msg:
         encoded_msg += huffman_code[letter]
     return encoded_msg, huffman_tree
 
 def huffman_decoding(msg,tree):
+    # Decode binary codes back into string message with huffman_tree
+    if tree == None:
+        # for an empty character (edge case)
+        return ''
     node = tree.get_root()
     decoded_msg = ''
     for digit in msg:
+        # digit '0' -> visit left_child
         if digit == '0':
             node = node.get_left_child()
+        # digit '1' -> visit right_child
         elif digit == '1':
             node = node.get_right_child()
-        else:
-            print("invalid binary digit")
+        # is_leaf node -> decode letter & restart from root_node
         if node.is_leaf():
             decoded_msg += node.get_name()
             node = tree.get_root()
@@ -147,22 +173,27 @@ def huffman_decoding(msg,tree):
 if __name__ == "__main__":
     msg_list = []
     msg_list += ["the quick brown fox jumps over a lazy dog"]
-    msg_list += ["the five boxing wizards jump quickly"]
-    msg_list += ["we promptly judged antique ivory buckles for the next prize"]
     msg_list += ["abcdeabcdabcaba"]
+    msg_list += ["www"]
+    msg_list += ["x"]
+    msg_list += [""]
 
     for msg in msg_list:
-        print ("The size of the data is: {}".format(sys.getsizeof(msg)))
-        print ("The content of the data is: {}\n".format(msg))
+        print("The size of the data is: {}".format(sys.getsizeof(msg)))
+        print("The content of the data is: {}\n".format(msg))
 
-        encoded_msg, tree = huffman_encoding(msg, True)
+        # set is_debug=True to print the debug output walkthrough
+        encoded_msg, tree = huffman_encoding(msg, is_debug=True)
+        # # leave it blank to print the normal output
         # encoded_msg, tree = huffman_encoding(msg)
-
-        print ("The size of the encoded data is: {}".format(sys.getsizeof(int(encoded_msg, base=2))))
-        print ("The content of the encoded data is: {}\n".format(encoded_msg))
+        if len(encoded_msg)>0:
+            print("The size of the encoded data is: {}".format(sys.getsizeof(int(encoded_msg, base=2))))
+            print("The content of the encoded data is: {}\n".format(encoded_msg))
+        # for an empty character (edge case)
+        else:
+            print("The content of the encoded data is: '' (empty string)\n")
 
         decoded_msg = huffman_decoding(encoded_msg, tree)
-
-        print ("The size of the decoded data is: {}".format(sys.getsizeof(decoded_msg)))
-        print ("The content of the encoded data is: {}\n".format(decoded_msg))
-        print ("==================================================\n")
+        print("The size of the decoded data is: {}".format(sys.getsizeof(decoded_msg)))
+        print("The content of the encoded data is: {}\n".format(decoded_msg))
+        print("==================================================\n")
--- problem3_Heap_Tree_BFS/test_huffman_coding.py
+++ problem3_Heap_Tree_BFS/test_huffman_coding.py
@@ -1,19 +1,35 @@
 from huffman_coding import *
 
-def test_pangram1():
+def test_pangram_sentence():
     msg = "the quick brown fox jumps over a lazy dog"
     encoded_msg, tree = huffman_encoding(msg, True)
     decoded_msg = huffman_decoding(encoded_msg, tree)
     assert(decoded_msg==msg)
 
-def test_pangram2():
     msg = "the five boxing wizards jump quickly"
     encoded_msg, tree = huffman_encoding(msg, True)
     decoded_msg = huffman_decoding(encoded_msg, tree)
     assert(decoded_msg==msg)
 
-def test_pangram3():
     msg = "we promptly judged antique ivory buckles for the next prize"
+    encoded_msg, tree = huffman_encoding(msg, True)
+    decoded_msg = huffman_decoding(encoded_msg, tree)
+    assert(decoded_msg==msg)
+
+def test_single_chartype():
+    msg = "www"
+    encoded_msg, tree = huffman_encoding(msg, True)
+    decoded_msg = huffman_decoding(encoded_msg, tree)
+    assert(decoded_msg==msg)
+
+def test_single_char():
+    msg = 'x'
+    encoded_msg, tree = huffman_encoding(msg, True)
+    decoded_msg = huffman_decoding(encoded_msg, tree)
+    assert(decoded_msg==msg)
+
+def test_empty_string():
+    msg = ''
     encoded_msg, tree = huffman_encoding(msg, True)
     decoded_msg = huffman_decoding(encoded_msg, tree)
     assert(decoded_msg==msg)
